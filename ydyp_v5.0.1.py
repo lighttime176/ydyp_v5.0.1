37e51793579c695fd89e618af327357abd20578e
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-11-12 15:09:48 +0800

    Update ydyp_v5.0.1.py

--- ydyp_v5.0.1.py
+++ ydyp_v5.0.1.py
@@ -825,7 +825,7 @@ class YP:
 
 
 if __name__ == "__main__":
-    env_name = 'Basic bW9iaWxlOjE5OTA5Njg3NTg1OkpwV0ZpeklRfDF8UkNTfDE3MzM5NzY5MTQzMzB8alF1bGI0czBpZ0Y5UGY0Y0lkMHNlVXpMTmJLVW1hNnhvX2pyR2tzNVl1VjRtWnFWQ1VBN2xwVFlPZk53Y21JenM5UGNzY0Ywc1A1LnNkak9xM0FYZDRNVW5kX2VKYXBHSkZ5cEoyeFpHcWJqRXRJVVZLanV1UjlhamdNbXNWUFB0ZlM1UlV3TTNTTnNCRmJ6aTV0dnhZYkl3a2QxQUNCWlpWdFJPZG9yU3JrLQ==#19909687585'
+    env_name = 'ydyp'
     token = os.environ.get("env_name")
     #token = os.getenv(env_name)
     if not token:
