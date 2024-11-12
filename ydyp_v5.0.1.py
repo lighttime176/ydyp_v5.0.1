3f2ff8ba84474df1d8804d21eac8a2455ac31950
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-11-12 15:04:44 +0800

    Update ydyp_v5.0.1.py

--- ydyp_v5.0.1.py
+++ ydyp_v5.0.1.py
@@ -826,7 +826,8 @@ class YP:
 
 if __name__ == "__main__":
     env_name = 'ydypCK'
-    token = os.getenv(env_name)
+    token = os.environ.get("env_name")
+    #token = os.getenv(env_name)
     if not token:
         print(f'⛔️未获取到ck变量：请检查变量 {env_name} 是否填写')
         exit(0)
